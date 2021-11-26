c46533a2fff5b0d7bb6dae0858500a0a848d7777
Isah Ohieku <isahohieku@gmail.com>
2021-11-26 16:14:32 +0100

    chore(*): Added services and components

---
+++ projects/clooper/common/ClooperCommon/visibility-toggle/visibility-toggle.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VisibilityToggleDirective } from './visibility-toggle.directive';
+
+describe('VisibilityToggleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VisibilityToggleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
